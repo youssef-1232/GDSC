1eb27464194a0422134b14d8a275cdcf56149794
youssef mohamed <84422783+youssef-1232@users.noreply.github.com>
2023-06-23 01:33:46 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 
 
-mongoose.connect('mongodb+srv://ym79793:a9ZoKqdFNfAuiA2z@atlasdb.in2rkqn.mongodb.net/atlasdb?retryWrites=true&w=majority')
+mongoose.connect('')
 
 .then(() => {
     console.log("concteded to atlas")
